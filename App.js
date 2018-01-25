60e2c3b3909cae446e2fb4ea8972f805abe29404
Matthew Carter <m@ahungry.com>
2018-01-24 23:37:14 -0500

    Work on a super simple app

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, Image, TextInput, ScrollView } from 'react-native';
+
+export default class App extends React.Component {
+  texter (msg) {
+    alert(msg)
+  }
+  render () {
+    return (
+      <ScrollView style={styles.container}>
+        <Image
+          source={{ uri: 'http://ahungry.com/img/logo.png' }}
+          style={{ width: 320, height: 280 }} />
+        <TextInput onChangeText={this.texter} />
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  contentContainerStyle: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
