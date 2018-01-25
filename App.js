9160063d18a23b4d0f29080504b3189f4a0c8ad2
Matthew Carter <m@ahungry.com>
2018-01-24 23:42:06 -0500

    Add a jsx comment

--- App.js
+++ App.js
@@ -8,13 +8,18 @@ export default class App extends React.Component {
   render () {
     return (
       <ScrollView style={styles.container}>
+
         <Image
           source={{ uri: 'http://ahungry.com/img/logo.png' }}
           style={{ width: 320, height: 280 }} />
+
+        {/* https://facebook.github.io/react-native/docs/textinput.html#onchangetext */}
         <TextInput onChangeText={this.texter} />
+
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
       </ScrollView>
     );
   }
